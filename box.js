995b056a1f499db824a8b51ce06cdf318a6b686c
YannickAiguier <yannick.aiguier@gmail.com>
2021-07-07 16:23:48 +0200

    Push distanciel : création chemin ok mais ne s'arrête pas, possibleNeighbors n'est pas vide

--- box.js
+++ box.js
@@ -11,25 +11,31 @@ class Box {
         this.possibleNeighbors.push([x, y]);
     }
 
-    findNeighbor(x, y) {
-        // indexOf
-    }
-
-    deleteNeighbor(i) {
-        this.possibleNeighbors.splice(i, 1);
-    }
-
     hasPossibleNeighbors() {
         return this.possibleNeighbors.length > 0;
     }
 
     chooseNeighbors() {
+        // tirage aléatoire de l'indice de la case choisie dans possibleNeighbors
+        console.log((this.possibleNeighbors.length));
         let nb = this.getRandomInt(this.possibleNeighbors.length);
-        return this.possibleNeighbors[nb];
+        // suppression de cete case du tableau en retour de son contenu
+        console.log(this.possibleNeighbors);
+        console.log(nb);
+        return this.possibleNeighbors.splice(nb, 1);
     }
 
     getRandomInt(max) {
-        return Math.floor(Math.random() * max) + 1;
+        let result = Math.floor(Math.random() * max);
+        if (result < max) {
+            return result;
+        } else {
+            return max - 1;
+        }
+    }
+
+    removeNeighbors() {
+        this.possibleNeighbors.length = 0;
     }
 }
 
--- generator.js
+++ generator.js
@@ -6,9 +6,6 @@ const Maze = require('./maze');
 const mazeWidth = 5;
 const mazeHeight = 5;
 
-// le tableau dans lequel on enregistre les cases visitées
-const visited = new Array();
-
 // le labyrinthe
 let myMaze = new Maze(mazeHeight, mazeWidth);
 
@@ -20,24 +17,47 @@ let y = 0;
 let endx = 3;
 let endy = 3;
 
-// initialiser le tableau des cases visitées, faux partout
-for (let i = 0; i < mazeHeight; i++) {
-    visited[i] = new Array(mazeWidth);
-    for (let j = 0; j < mazeWidth; j++) {
-        visited[i][j] = false;
-    }
-}
-console.table(visited);
+console.log("myMaze :");
+console.log(myMaze);
 
 ////////
 // Début du programme
 ////////
 
+generateBox([0, 0]);
 
 
 console.log(myMaze);
-console.log(myMaze.getBox(0,0));
-console.log(myMaze.getBox(0,0).hasPossibleNeighbors());
-console.log(myMaze.isPossible(0,0));
-myMaze.findPossibleNeighbors(0,0);
-console.log(myMaze.getBox(0,0).possibleNeighbors);
+
+
+////////
+// Fin du programme
+////////
+
+function generateBox([x, y]) {
+    // ajout de la case courante à la liste des cases visitées
+    myMaze.setVisited(x, y);
+    console.log("Visited :");
+    console.table(myMaze.visited);
+    myMaze.findPossibleNeighbors(x, y);
+    console.log("Possible Neighbors :");
+    console.table(myMaze.getBox(x, y).possibleNeighbors);
+    console.log("hasPossibleNeighbors :");
+    console.log(myMaze.getBox(x, y).hasPossibleNeighbors());
+    if (myMaze.getBox(x, y).hasPossibleNeighbors()) {
+        // tant qu'il y a une case disponible
+        while (myMaze.getBox(x, y).hasPossibleNeighbors()) {
+            // tirer au sort une case dispo
+            let nextBoxIndex = myMaze.getBox(x, y).chooseNeighbors();
+            // recommencer avec cette case
+            // console.log pour vérifier valeur retournée ?
+            console.log("nextBoxIndex :");
+            console.table(nextBoxIndex);
+            generateBox(nextBoxIndex[0]);
+        }
+    } else {
+        // plus de case dispo = cul de sac, rien à faire
+        console.log("Stop !");
+        return;
+    }
+}
